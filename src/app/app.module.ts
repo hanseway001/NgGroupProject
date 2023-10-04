e69f8cc8850b74284971653174a7ec47fcdbfe51
Tegan <teganator275@gmail.com>
2023-10-04 10:44:36 -0600

    completed oauth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './modules/material.module';
-import { environment } from 'environment';
+
+
 import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from "../../environment";
+
 import { FormsModule } from '@angular/forms';
 import { ComponentsComponent } from './components/components.component';
 import { FirstPageComponent } from './first-page/first-page.component';
@@ -15,27 +21,39 @@ import { SearchMoviesComponent } from './components/search-movies/search-movies.
 import { TopNavComponent } from './components/top-nav/top-nav.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { LoginPageComponent } from '../app/login-page/login-page.component';
+
+
+// service
+// import { AuthService } from './shared/services/auth.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchMoviesComponent,
     TopNavComponent,
-    MovieDetailsComponent
+    MovieDetailsComponent,
+    LoginPageComponent,
+  ],
+  exports: [
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
     FormsModule,
     HttpClientModule,
+
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
     // HttpClient,
     // HttpModule
   ],
-  providers: [],
+  providers: [LoginPageComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -2,9 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SearchMoviesComponent } from '../components/search-movies/search-movies.component';
 import { MovieDetailsComponent } from '../components/movie-details/movie-details.component';
+import { LoginPageComponent } from '../login-page/login-page.component';
+
+import { SignInComponent } from '../components/sign-in/sign-in.component';
+import { SignUpComponent } from '../components/sign-up/sign-up.component';
+import { VerifyEmailComponent } from '../components/verify-email/verify-email.component';
+
 
 const routes: Routes = [
   // {path: '', component: },
+  
+  { path: '', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'verify-email-address', component: VerifyEmailComponent },
+
+  {path: 'login', component: LoginPageComponent },
   {path: 'search', component: SearchMoviesComponent},
   // {path: 'movie-details/**', component: MovieDetailsComponent},
   {path: 'movie-details/:movieId', component: MovieDetailsComponent},
